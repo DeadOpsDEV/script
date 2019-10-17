5aeb6a5298237ed0bab2b8bcea71263b5603d158
DeadOps <loic.barel34@gmail.com>
2019-10-17 20:36:56 +0200

    17/10/19

--- scripta.sh
+++ scripta.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-destination=`cat liste_dest`
+liste_destination=`cat liste_dest`
 
 echo "" > collecte/icmp_route
 echo "" > collecte/tcp_route
 echo "" > collecte/udp_route
 
-for i in $destination 
+for dest in $liste_destination 
 do
-	
-	for ((nbHop=0 ; nbHop<30 ; nbHop++))
-	do
-		test=`traceroute -I -f $nbHop -m $nbHop -q 1 -w 20 $i|grep -v "traceroute"|sed -e "s/ /,/g"|cut -d "," -f4`
-		if [ $destination -eq $test ]
-		then
-			echo "$destination = $test"	 
-		fi
-	done  
-		
+	for protocole in "-I" "-U" "-T"; do
+		for ((nbHop=1 ; nbHop<=30 ; nbHop++))
+		do
+			test=`traceroute $protocole $dest -f $nbHop -m $nbHop -q 1 -w 20|grep -v "traceroute"|awk '{print $2}'`
+			echo $test >> temp 
+
+			if [ "$test" == "$dest" ]; then
+				echo "[$protocole] $dest == > $test ($nbHop)"
+				if [ `cat temp|grep "*"` ]; then
+					echo "changes les ports"
+				fi
+				break
+			fi	
+		done
+		#echo "" > temp   
+	done	
 
 done  
